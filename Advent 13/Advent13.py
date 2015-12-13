403321c6b619eff35850f4fdb85d48c159037b6e
vinheim3 <vinheim3@yahoo.co.uk>
2015-12-13 17:49:02 +0000

    Day 13 solution
    Non-brute force solution. All permutations are unique, as keeping a
    person in the same place in the list prevents 1 permutation from being a
    rotated version of another permutation.

---
+++ Advent 13/Advent13.py
@@ -0,0 +1,44 @@
+import re
+from itertools import permutations
+
+data=open("Advent13.txt").read()
+
+happiness=dict()
+people=set()
+
+m=re.compile("([a-zA-z]+) would (gain|lose) (\d+) happiness units by sitting next to ([a-zA-z]+)")
+
+for i in data.split("\n"):
+    p = m.search(i)
+    P1=p.group(1)
+    P2=p.group(4)
+    gain=int(p.group(3))*(2*(p.group(2)=="gain")-1)
+    
+    people.add(P1)
+    people.add(P2)
+
+    for i in [(P1,P2),(P2,P1)]:
+        if i in happiness:
+            happiness[i]+=gain
+        else:
+            happiness[i]=gain
+
+#uncomment the following 4 lines for part 2
+#for i in people:
+#    happiness[(i,"Me")]=0
+#    happiness[("Me",i)]=0
+#people.add("Me")
+
+fixedP=people.pop()
+maxHappiness=0
+
+for i in permutations(people):
+    i=list(i)
+    i.append(fixedP)
+    
+    currHappiness=happiness[(i[len(people)],i[0])]
+    currHappiness+=sum(map(lambda x,y:happiness[(x,y)],i[:-1],i[1:]))
+    
+    maxHappiness=max(currHappiness,maxHappiness)
+
+print maxHappiness
