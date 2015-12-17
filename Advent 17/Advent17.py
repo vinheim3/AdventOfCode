caef8f021d867e77a17ae71b8a132608a983a7fe
vinheim3 <vinheim3@yahoo.co.uk>
2015-12-17 06:24:51 +0000

    Day 17 refinement
    Once it finds a solution, it stops counting if the next solution uses 1
    more jug

--- Advent 17/Advent17.py
+++ Advent 17/Advent17.py
@@ -2,20 +2,14 @@ import itertools
 
 data=open("Advent17.txt").read().split("\n")
 count=0
-mini=0
+found=False
 
-#for part 2, uncomment everything and comment out count+=1 before mini=j
 for j in range(len(data)):
+    if found==True:
+        break
     for i in itertools.combinations(data, j):
         if sum(int(x) for x in i)==150:
             count+=1
-            #mini=j
-            #break
-    if mini>0:
-        break
-
-#for i in itertools.combinations(data, mini):
-#    if sum(int(x) for x in i)==150:
-#        count+=1
+            #found=True
 
 print count
